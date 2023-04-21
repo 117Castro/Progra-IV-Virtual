af54603c7bceea56e2ae148865b402be0ea70257
Carlos Castro <carloseduardocastrogiron@gmail.com>
2023-04-20 21:53:21 -0500

    modificado

--- Procesar_datos_alumnos.php
+++ Procesar_datos_alumnos.php
@@ -1,10 +1,22 @@
 <?php
-$nombre = '';
-$direccion = ''; 
+$nombre = $_POST [ 'txtnombre' ];
+$direccion = $_POST [ 'txtdireccion' ];
+$edad = $_POST [ 'txtedad' ];
+$mensaje = '';
 
+if ( $edad >= 1 && $edad <= 2 ){
+    $msj = 'Eres un bebe' ;
+} else  if ( $edad <= 11 ){
+    $msj = 'Eres un niño' ;
+} else  if ( $edad <= 17 ){
+    $msj = 'Eres un adolescente' ;
+} else  if ( $edad <= 65 ){
+    $msg = 'Eres un adulto, tienes responsabilidades' ;
+} else  if ( $edad > 65 ){
+    $mensaje = 'Larga Vida' ;
+} más {
+    $msg = 'Error en la edad' ;
+}
 
-
-echo "hola $nombre";
-
-
+echo " Hola $nombre ; vives en $direccion ; tu edad es: $edad ; $msj ";
 ?>
