f00e38e8b1815107ab4c688c32a6de963aee10f3
Carlos Castro <carloseduardocastrogiron@gmail.com>
2023-04-20 22:08:31 -0500

    modificado

--- Procesar_datos_alumnos.php
+++ Procesar_datos_alumnos.php
@@ -1,22 +1,22 @@
 <?php
-$nombre = $_POST [ 'txtnombre' ];
-$direccion = $_POST [ 'txtdireccion' ];
-$edad = $_POST [ 'txtedad' ];
-$mensaje = '';
+$nombre = $_POST['txtnombre'];
+$direccion = $_POST['txtdireccion'];
+$edad = $_POST['txtedad'];
+$msg = '';
 
-if ( $edad >= 1 && $edad <= 2 ){
-    $msj = 'Eres un bebe' ;
-} else  if ( $edad <= 11 ){
-    $msj = 'Eres un niño' ;
-} else  if ( $edad <= 17 ){
-    $msj = 'Eres un adolescente' ;
-} else  if ( $edad <= 65 ){
-    $msg = 'Eres un adulto, tienes responsabilidades' ;
-} else  if ( $edad > 65 ){
-    $mensaje = 'Larga Vida' ;
-} más {
-    $msg = 'Error en la edad' ;
+if($edad>=1 && $edad<=2){
+    $msg = 'Eres un bebe';
+}else if($edad<=11){
+    $msg='Eres un niño';
+}else if($edad<=17){
+    $msg = 'Eres un adolescente';
+}else if($edad<=65){
+    $msg='Eres un adulto, tienes responsabilidades';
+}else if($edad>65){
+    $msg = 'Larga Vida';
+}else{
+    $msg = 'Error en la edad';
 }
 
-echo " Hola $nombre ; vives en $direccion ; tu edad es: $edad ; $msj ";
+echo "Hola $nombre; vives en $direccion; tu edad es: $edad; $msg";
 ?>
