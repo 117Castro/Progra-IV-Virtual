8c5c436835d474e795aa40eebbab84a55cdc7bbc
Carlos Castro <carloseduardocastrogiron@gmail.com>
2023-04-23 20:37:05 -0500

    act

--- Calculadora_de_compras.php
+++ Calculadora_de_compras.php
@@ -11,13 +11,13 @@
 	<form method="post" action="resultado.php">
 		<label for="nombre">Nombre del producto:</label>
 		<input type="text" id="nombre" name="nombre" required>
-		<br>
+		<br><br>
 		<label for="cantidad">Cantidad de productos:</label>
 		<input type="number" id="cantidad" name="cantidad" min="1" required>
-		<br>
-		<label for="precio">Precio del producto:</label>
+		<br><br>
+		<label for="precio">Precio del producto: </label>
 		<input type="number" id="precio" name="precio" min="0" step="0.01" required>
-		<br>
+		<br><br>
 		<button type="submit">Calcular</button>
 	</form>
 </body>
